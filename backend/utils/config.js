e97cf625cf19e5e532c838a0258c634b52d922a9
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-09-09 17:40:15 +0300

    fix: no prt

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL || 'mongodb://localhost:27017/mestodb',
+  databaseUrl: process.env.DATABASE_URL || 'mongodb://localhost/mestodb',
   jwtSecret: process.env.JWT_SECRET || '',
 };
 
