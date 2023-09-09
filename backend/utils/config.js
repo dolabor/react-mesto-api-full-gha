0c200d1f1fee18d1cf10d264ca64cba20435e8cf
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-09-09 15:37:31 +0300

    fix: logger updated

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL || 'mongodb://158.160.46.184/mestodb',
+  databaseUrl: process.env.DATABASE_URL || 'mongodb://localhost:27017/mestodb',
   jwtSecret: process.env.JWT_SECRET || '',
 };
 
